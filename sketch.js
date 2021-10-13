5b7c07b29c71043dad9a208bfcddd4e2cd322bb0
carGuy77 <88950581+carGuy77@users.noreply.github.com>
2021-10-12 22:17:45 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,20 @@
-var bow , arrow,  background;
+var PLAY = 1;
+var END = 0;
+var gameState = PLAY;
+var bow , arrow,  background, redB, pinkB, greenB ,blueB ,arrowGroup;
 var bowImage, arrowImage, green_balloonImage, red_balloonImage, pink_balloonImage ,blue_balloonImage, backgroundImage;
 
+var score =0;
 function preload(){
   
   backgroundImage = loadImage("background0.png");
+  
   arrowImage = loadImage("arrow0.png");
   bowImage = loadImage("bow0.png");
   red_balloonImage = loadImage("red_balloon0.png");
+  green_balloonImage = loadImage("green_balloon0.png");
+  pink_balloonImage = loadImage("pink_balloon0.png");
+  blue_balloonImage = loadImage("blue_balloon0.png");
   
 }
 
@@ -25,38 +33,137 @@ function setup() {
   bow.addImage(bowImage); 
   bow.scale = 1;
   
+   score = 0  
+ //redB= new Group();
+ 
+ // arrowGroup= new Group();
+
+  
 }
 
 function draw() {
  background(0);
-  // moving ground
-    scene.velocityX = -3 
+ 
 
-    if (scene.x < 0){
-      scene.x = scene.width/2;
-    }
-  
-  //moving bow
-  bow.y = World.mouseY
+ 
+ if(gameState === PLAY)
+ {
+      /*Uncomment correct option 
+        according to PLAY state*/  
+      // // moving ground
+      // scene.velocityX = -3 
+      // //destroy bow
+      // bow.destroy();
+      // //reset the background
+      // if (scene.x < 0){
+      //     scene.x = scene.width/2;
+      //    }
+      // //moving bow
+      // bow.y = World.mouseY      
+      // //stop background movement
+      // scene.velocityX = 0;
+
   
    // release arrow when space key is pressed
   if (keyDown("space")) {
-    createArrow();
-    
+    createArrow();  
   }
   
-  //creating continous balloons
-  var select_balloon = Math.round(random(1,1));
+  //creating continous enemies
+  var select_balloon = Math.round(random(1,4));
   
   if (World.frameCount % 100 == 0) {
-    if (select_balloon == 1) {
-      redBalloon();
+    switch(select_balloon ){
+      case 1: redBalloon();
+      break;
+      case 2:blueBalloon();
+      break;
+      case 3:pinkBalloon();
+      break;
+      case 4:greenBalloon();
+      break;
+      default:break;
     }
   }
-  
-  drawSprites();
+ }
+
+
+  if (gameState === END) {
+    /*Uncomment correct option 
+      according to END state*/  
+      // // moving ground
+      // scene.velocityX = -3 
+      // //destroy bow
+      // bow.destroy();
+      // //reset the background
+      // if (scene.x < 0){
+      //     scene.x = scene.width/2;
+      //    }
+      // //moving bow
+      // bow.y = World.mouseY      
+      // //stop background movement
+      // scene.velocityX = 0;
+
+  }
+
+if (frameCount>1000) {
+  //red.destroyEach();
+  gameState=END; 
 }
 
+
+
+
+ 
+  
+  drawSprites();
+  text("Score: "+ score, 300,50);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function redBalloon() {
+  var red = createSprite(0,Math.round(random(20, 370)), 10, 10);
+  red.addImage(red_balloonImage);
+  red.velocityX = 3;
+  red.lifetime = 150;
+  red.scale = 0.1;
+//  redB.add(red);
+}
+
+function blueBalloon() {
+  var blue = createSprite(0,Math.round(random(20, 370)), 10, 10);
+  blue.addImage(blue_balloonImage);
+  blue.velocityX = 3;
+  blue.lifetime = 150;
+  blue.scale = 0.1;
+}
+
+function greenBalloon() {
+  var green = createSprite(0,Math.round(random(20, 370)), 10, 10);
+  green.addImage(green_balloonImage);
+  green.velocityX = 3;
+  green.lifetime = 150;
+  green.scale = 0.1;
+}
+
+function pinkBalloon() {
+  var pink = createSprite(0,Math.round(random(20, 370)), 10, 10);
+  pink.addImage(pink_balloonImage);
+  pink.velocityX = 3;
+  pink.lifetime = 150;
+  pink.scale = 1
+
+}
 
 // Creating  arrows for bow
  function createArrow() {
@@ -67,26 +174,6 @@ function draw() {
   arrow.velocityX = -4;
   arrow.lifetime = 100;
   arrow.scale = 0.3;
-}
+//  arrowGroup.add(arrow);
 
-
-function redBalloon() {
-  var red = createSprite(0,Math.round(random(20, 370)), 10, 10);
-  red.addImage(red_balloonImage);
-  red.velocityX = 3;
-  red.lifetime = 150;
-  red.scale = 0.1;
-
-}
-
-function blueBalloon() {
-  //write code for spwaning blue balloons
-}
-
-function greenBalloon() {
-  //write code for spwaning green balloons
-}
-
-function pinkBalloon() {
-  //write code for spwaning pink balloons
 }
